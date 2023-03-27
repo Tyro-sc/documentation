28171bdbdd87074ee2c373da87abec329dec6301
altus34 <d.avenante@gmail.com>
2023-03-26 22:58:14 -0400

    Setup for CI usage

--- src/test/groovy/sc/tyro/doc/TyroExtension.groovy
+++ src/test/groovy/sc/tyro/doc/TyroExtension.groovy
@@ -8,6 +8,7 @@ import org.junit.jupiter.api.extension.ExtensionContext
 import org.openqa.selenium.WebDriver
 import org.openqa.selenium.chrome.ChromeOptions
 import org.openqa.selenium.firefox.FirefoxOptions
+import org.slf4j.Logger
 import sc.tyro.web.WebBundle
 
 import static io.github.bonigarcia.wdm.WebDriverManager.chromedriver
@@ -15,8 +16,12 @@ import static io.github.bonigarcia.wdm.WebDriverManager.firefoxdriver
 import static io.javalin.http.staticfiles.Location.CLASSPATH
 import static java.lang.Boolean.valueOf
 import static java.lang.System.getenv
+import static java.net.InetAddress.getByName
+import static org.slf4j.LoggerFactory.getLogger
 
 class TyroExtension implements BeforeAllCallback, AfterAllCallback {
+    private static Logger LOGGER = getLogger(TyroExtension)
+
     public static String BASE_URL
     private static Javalin app
     private static WebDriver webDriver
@@ -27,16 +32,19 @@ class TyroExtension implements BeforeAllCallback, AfterAllCallback {
     void beforeAll(ExtensionContext extensionContext) throws Exception {
         app = Javalin.create({it.staticFiles.add "/webapp", CLASSPATH }).start(0)
 
-        BASE_URL = "http://localhost:${app.port()}"
+        DatagramSocket socket = new DatagramSocket()
+        socket.connect(getByName("8.8.8.8"), 10002)
+        String host_ip = socket.localAddress.hostAddress
+        BASE_URL = "http://${host_ip}:${app.port()}/"
 
         // Add -Dbrowser=firefox/chrome/... to you VM Option to select the browser
         String browser = System.getProperty("browser")
-        System.setProperty("webdriver.http.factory", "jdk-http-client")
         if (!browser) {
-            println "No browser selected. Use Chrome"
+            LOGGER.info('No Browser selected. Use Chrome')
             browser = "chrome"
         }
 
+        System.setProperty("webdriver.http.factory", "jdk-http-client")
         switch (browser) {
             case "firefox":
                 wdm = firefoxdriver()
@@ -50,11 +58,10 @@ class TyroExtension implements BeforeAllCallback, AfterAllCallback {
                 break
             default:
                 throw new IllegalStateException("Fail to set browser: " + browser)
-
         }
 
         if (isCI) {
-            wdm.browserInDocker()
+            wdm.browserInDocker().enableVnc()
         }
 
         webDriver = wdm.create()
